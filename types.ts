397b8856ac2377dc3a131cf84182cf2679bc264d
Marek Kramarz <markoni@spoko.pl>
2025-01-29 22:56:31 +0100

    chore: update project dependencies and scripts
    feat: add types for Muscle, Exercise, and Training
    
    chore: add tsconfig.json for TypeScript configuration
    
    chore: update .gitignore
    
    chore: update icon.png asset

---
+++ types.ts
@@ -0,0 +1,19 @@
+export type Muscle = {
+  id: number;
+  name: string;
+};
+
+export type Exercise = {
+  id: number;
+  name: string;
+  involvedMuscles: Muscle[];
+  lastPerformed: string;
+};
+
+export type Training = {
+  id: number;
+  name: string;
+  date: string;
+  isCompleted: boolean;
+  exercises: Exercise[];
+};
